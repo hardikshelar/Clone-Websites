07fe90fb57ece4fb2e0899baa7c17df60e2226b6
Hardik Shelar <90783840+hardikshelar@users.noreply.github.com>
2024-01-13 13:28:53 +0530

    Add files via upload

---
+++ royal.js
@@ -0,0 +1,76 @@
+function fn(UserInput){
+    if(UserInput=='Bullet'){
+            document.getElementById("motorCycleimg").src="./Images/C3/2.jpg";
+        }
+    
+    else if(UserInput=='Himalayan'){
+    document.getElementById("motorCycleimg").src="./Images/C3/1.jpg";
+}  
+else if(UserInput=='Interceptor'){
+    document.getElementById("motorCycleimg").src="./Images/C3/3.avif";
+}  
+else if(UserInput=='Continental'){
+    document.getElementById("motorCycleimg").src="./Images/C3/4.jpg";
+}  
+else if(UserInput=='SuperMateor'){
+    document.getElementById("motorCycleimg").src="Images/C3/5.jpg";
+}  
+else if(UserInput=='Hunter'){
+    document.getElementById("motorCycleimg").src="Images/C3/6.jpg";
+} 
+else if(UserInput=='Scram'){
+    document.getElementById("motorCycleimg").src="./Images/C3/7.avif";
+} 
+else if(UserInput=='Classic'){
+    document.getElementById("motorCycleimg").src="Images/C3/8.avif";
+} 
+else if(UserInput=='Mateor'){
+    document.getElementById("motorCycleimg").src="Images/C3/9.jpg";
+} 
+else if(UserInput=='Motoverse'){
+    document.getElementById("Rideimg").src="./Images/C4/1.avif";
+}
+else if(UserInput=='Track'){
+    document.getElementById("Rideimg").src="./Images/C4/2.jpg";
+}
+else if(UserInput=='GT'){
+    document.getElementById("Rideimg").src="./Images/C4/3.avif";
+}
+else if(UserInput=='Rentals'){
+    document.getElementById("Rideimg").src="./Images/C4/4.jpg";
+}
+else if(UserInput=='Mapped'){
+    document.getElementById("Rideimg").src="./Images/C4/5.jpg";
+}
+else if(UserInput=='Leh'){
+    document.getElementById("Rideimg").src="./Images/C4/6.jpg";
+}
+else if(UserInput=='Goa'){
+    document.getElementById("Rideimg").src="./Images/C4/7.jpg";
+}
+else if(UserInput=='REXA'){
+    document.getElementById("ApparelImg").src="./Images/C5/1.jpg";
+}
+else if(UserInput=='REXT'){
+    document.getElementById("ApparelImg").src="./Images/C5/2.avif";
+}
+else if(UserInput=='MIY'){
+    document.getElementById("ApparelImg").src="./Images/C5/3.avif";
+}
+else if(UserInput=='GMA'){
+    document.getElementById("AccessoriesImg").src="./Images/C6/1.jpg";
+}
+else if(UserInput=='MIY2'){
+    document.getElementById("AccessoriesImg").src="./Images/C6/2.jpg";
+}
+else if(UserInput=='Stores'){
+    document.getElementById("LocateUsImg").src="./Images/C7/1.jpg";
+}
+else if(UserInput=='Service'){
+    document.getElementById("LocateUsImg").src="./Images/C7/2.jpg";
+}
+else if(UserInput=='Distributer'){
+    document.getElementById("LocateUsImg").src="./Images/C7/3.avif";
+}
+}
+
